b223b0dd11216895e7f05761aa81b64467b45900
ladylike778 <105021037@live.asia.edu.tw>
2016-11-20 21:00:15 +0800

    finish3

--- src/hw/hw03.java
+++ src/hw/hw03.java
@@ -1,9 +1,20 @@
 package hw;
-
+import java.util.*;
 public class hw03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn=new Scanner(System.in);
+		int m=scn.nextInt();
+		int n=scn.nextInt();
+		System.out.print(C(m)/C(n)/C(m-n));
+		}
+	public static int C(int v){
+		if(v==1){
+			return 1;
+		}else{
+			return v * C(v-1);
+		}
 
 	}
 
